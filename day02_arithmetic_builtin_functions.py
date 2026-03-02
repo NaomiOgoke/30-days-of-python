af20db9abff36f95263da2109605e2717aa7b85f
NaomiOgoke <100643532+NaomiOgoke@users.noreply.github.com>
2026-03-02 21:58:31 +0100

    Day 2: Completed arithmetic operations exercises

---
+++ day02_arithmetic_builtin_functions.py
@@ -0,0 +1,36 @@
+# Day 2: 30 Days of python programming
+first_name = 'Naomi'
+last_name = 'Ogoke'
+full_name = 'Naomi Ogoke'
+country = 'Nigeria'
+city = 'Owerri'
+age = 25
+year = 2026
+is_married = False
+is_true = True
+is_light_on = False
+skills = ['HTML', 'CSS', 'JS', 'React', 'Python']
+len('Naomi')
+5
+len('Ogoke')
+5
+num_one = 5
+num_two = 4
+total = num_one +num_two
+print(total)
+9
+diff = num_one - num_two
+print(diff)
+1
+product = num_one * num_two
+print(product)
+20
+division = num_one / num_two
+print(division)
+1.25
+remainder = num_one % num_two
+print(remainder)
+1
+exp = num_one ** num_two
+print(exp)
+625
